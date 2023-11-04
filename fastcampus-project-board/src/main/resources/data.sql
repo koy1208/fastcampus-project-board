5c6078bf6e89cd56ba1b0148a5a7f6b935085314
koy1208 <kkoy120806@gmail.com>
2023-11-04 23:18:38 +0900

    #19 - 코드 수정
    입력 데이터 수정

--- fastcampus-project-board/src/main/resources/data.sql
+++ fastcampus-project-board/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('uno', 'asdf1234', 'Uno', 'uno@mail.com', 'I am Uno.', now(), 'uno', now(), 'uno')
+    ('koy1208', 'asdf1234', 'koy1208', 'koy1208@mail.com', 'I am koy1208.', now(), 'koy1208', now(), 'koy1208')
 ;
 
 -- 123 게시글
